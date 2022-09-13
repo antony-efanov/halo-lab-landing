51b48c37d7dd16defd87d6ee16fedb04ec877021
Anton Yefanov <antonyefanov@gmail.com>
2022-09-13 18:50:44 +0300

    team design done

---
+++ src/components/Team/Team.tsx
@@ -0,0 +1,33 @@
+import TeamMember1 from "../../assets/Team/TeamMember1.webp";
+import TeamMember2 from "../../assets/Team/TeamMember2.webp";
+import TeamMember3 from "../../assets/Team/TeamMember3.webp";
+
+const Team = () => {
+  return (
+    <section className="team">
+      <h2 className="title">Our Top Team</h2>
+      <p className="description">
+        Learn more about how you can save our planet's nature.
+      </p>
+      <div className="team__members">
+        <img
+          className="team__members--memberOne"
+          src={TeamMember1}
+          alt="TeamMember1"
+        />
+        <img
+          className="team__members--memberTwo"
+          src={TeamMember2}
+          alt="TeamMember2"
+        />
+        <img
+          className="team__members--memberThree"
+          src={TeamMember3}
+          alt="TeamMember3"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Team;
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,11 +1,13 @@
 import { Header } from "../../components/Header";
 import { Hero } from "../../components/Hero";
+import { Team } from "../../components/Team";
 
 const Main = () => {
   return (
     <div className="app">
       <Header />
       <Hero />
+      <Team />
     </div>
   );
 };
